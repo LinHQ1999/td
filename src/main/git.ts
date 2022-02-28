ea652f257b68afadb15c4bd88f4d0eeaae417452
LinHQ1999 <1157136867@qq.com>
2022-02-28 21:37:29 +0800

    解决 git 操作 bug

--- src/main/git.ts
+++ src/main/git.ts
@@ -1,24 +1,27 @@
-import { exec } from 'child_process'
-import { info } from 'electron-log'
+import { exec as _exec } from 'child_process'
+import { info, error } from 'electron-log'
 import { existsSync } from 'original-fs'
+import { join } from 'path'
+import { promisify } from 'util'
 
 /**
  * 检查 git 状态并提交
  * @param dir 工作目录
  */
 export function CheckCommit(dir: string): void {
-    if (existsSync(".git")) {
-        exec("git status", { cwd: dir }, (err, out, stderr) => {
-            if (out.toString().split("\n")[0] != "") {
-                exec(`git add . && git commit -a -m "${new Date().toLocaleString()}"`, { cwd: dir, timeout: 5000 },
-                    (error, out, stderr) => {
-                        if (!error)
-                            info("自动提交成功")
-                        else
-                            info(stderr)
-                    })
-            } else info("无变更")
-        })
+    let exec = promisify(_exec)
+    if (existsSync(join(dir, ".git"))) {
+        exec("git status -s", { cwd: dir })
+            .then(({ stdout, stderr }) => {
+                if (stdout.toString().split("\n")[0] != "") {
+                    exec(`git add . && git commit -a -m "${new Date().toLocaleString()}"`, { cwd: dir, timeout: 5000 })
+                        .then(({ stdout, stderr }) => {
+                            if (!stderr) info(stdout)
+                        })
+                        .catch(error)
+                }
+            })
+            .catch(error)
     } else {
         info("无仓库")
     }
