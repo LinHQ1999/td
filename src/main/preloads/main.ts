4391776da5ae1ae43a6927c8cc4ab1844a5deae5
LinHanqing <linhanqing01943@os-easy.com>
2022-08-07 23:29:21 +0800

    继续尝试加搜索

--- src/main/preloads/main.ts
+++ src/main/preloads/main.ts
@@ -1,20 +1,26 @@
-import {contextBridge, ipcRenderer} from "electron"
+import { contextBridge, ipcRenderer } from "electron";
 
 export interface FileInfo {
-    name: string
-    path: string
+  name: string;
+  path: string;
 }
 
 export const api = {
-    "import": (info: FileInfo) => ipcRenderer.invoke("import", info),
-    "gc": (files: string[]) => ipcRenderer.invoke("gc", files),
-    "download": (file: ArrayBuffer, fname: string) => ipcRenderer.invoke("download", file, fname),
-    "convert": (file: string, fname: string) => ipcRenderer.invoke("convert", file, fname),
-    "delete": (canonical: string) => ipcRenderer.invoke("delete", canonical),
-    "save": (abspath: string, text: string) => ipcRenderer.invoke("save", abspath, text),
-    "savesync": (abspath: string, text: string) => ipcRenderer.sendSync("savesync", abspath, text),
-    "confirm": (msg: string) => ipcRenderer.sendSync("confirm", msg),
-    "alert": (msg: string) => ipcRenderer.sendSync("alert", msg)
-}
+  import: (info: FileInfo) => ipcRenderer.invoke("import", info),
+  gc: (files: string[]) => ipcRenderer.invoke("gc", files),
+  download: (file: ArrayBuffer, fname: string) =>
+    ipcRenderer.invoke("download", file, fname),
+  convert: (file: string, fname: string) =>
+    ipcRenderer.invoke("convert", file, fname),
+  delete: (canonical: string) => ipcRenderer.invoke("delete", canonical),
+  save: (abspath: string, text: string) =>
+    ipcRenderer.invoke("save", abspath, text),
+  savesync: (abspath: string, text: string) =>
+    ipcRenderer.sendSync("savesync", abspath, text),
+  confirm: (msg: string) => ipcRenderer.sendSync("confirm", msg),
+  alert: (msg: string) => ipcRenderer.sendSync("alert", msg),
+  search: (text: string, mode?: -1 | 0 | 1) =>
+    ipcRenderer.invoke("search", text, mode),
+};
 
-contextBridge.exposeInMainWorld("TD", api)
+contextBridge.exposeInMainWorld("TD", api);
